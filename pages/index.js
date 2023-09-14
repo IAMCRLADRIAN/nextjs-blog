9bd04c5a5297eda46ebcc39a51f0e485c52a4990
IAMCRLADRIAN <143055462+IAMCRLADRIAN@users.noreply.github.com>
2023-09-14 10:47:08 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am Carl Adrian M Mendoza, A part-time Draftsman. </p>
         <p>
+    <p>I am Carl Adrian M Mendoza, A part-time Draftsman. </p>
+        <p>10 Things That Require Zero Talent</p>
+          <li>1. Being On Time</li>
+          <li>2. Making An Effort</li>
+          <li>3. Being High Energy</li>
+          <li>4. Having A Positive Attitude</li>
+          <li>5. Being Passionate</li>
+          <li>6. Using Good Body Language</li>
+          <li>7. Being Coachable</li>
+          <li>8. Doing A Little Extra</li>
+          <li>9. Being Prepared</li>
+          <li>10. Having A Strong Work Ethic</li>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
